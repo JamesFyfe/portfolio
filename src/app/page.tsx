90f74b3527fed596caa67f90a1dde5c6177bbeb2
JamesFyfe <139155049+de-jamesfyfe@users.noreply.github.com>
2024-04-16 22:59:34 -0700

    branch test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      This is a test
+      This is a branch test
     </main>
   );
 }
