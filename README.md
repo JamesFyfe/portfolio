294256a6ae689fdc0ab302f7862110a0a0255202
JamesFyfe <139155049+de-jamesfyfe@users.noreply.github.com>
2024-04-24 16:43:38 -0700

    set up initial page structure

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-My portfolio site
+# My portfolio
+
+## TODO
+
+- [x] deploy (vercel)
+- [x] set up initial page structure
+- [ ] build left intro/nav area
+- [ ] build right main content
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { LeftSide } from "~/components/leftSide";
+import { MainContent } from "~/components/mainContent";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      This is a test
+    <main className="grid grid-cols-1 md:grid-cols-5 h-screen max-w-7xl m-auto gap-32">
+      <div className="md:col-span-2">
+        <LeftSide />
+      </div>
+      <div className="md:col-span-3 overflow-y-auto">
+        <MainContent />
+      </div>
     </main>
   );
 }
